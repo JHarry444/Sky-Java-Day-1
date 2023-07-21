fca306e4955b08ebc3b5516a7d86bbd98dbacda2
jordan <jharrison1@qa.com>
2023-07-21 14:09:14 +0100

    Init commit

---
+++ src/com/qa/demo/Person.java
@@ -0,0 +1,4 @@
+package com.qa.demo;
+
+public class Person {
+}
---
+++ src/com/qa/demo/Runner.java
@@ -0,0 +1,45 @@
+package com.qa.demo;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        byte b= 122;
+        short s = 300;
+//        s = "test";
+        int i = 2_000_000_000;
+        long  l = 999_999_999_999L;
+
+        float f = 44.94F;
+        double d = 999.999;
+
+        char c  = 'j';
+
+        boolean bool = true;
+
+        String str = "Jordan";
+
+        System.out.println(str + " Harrison");
+        System.out.println(c + 'h');
+
+        int num1 = 12;
+        int num2 = 12;
+
+        System.out.println(12 == 12);
+        System.out.println(num1 == num1);
+        System.out.println(num1 == num2);
+
+        System.out.println("Strings: " + ("Jordan" == "Jordan"));
+        String name1 = "Jordan";
+        String name2 = "Jordan";
+        System.out.println("Strings: " + (name1 == name2));
+
+        System.out.println("Strings: " + (new String("Jordan") == "Jordan"));
+
+        System.out.println(new String("Jordan").equals("Jordan"));
+
+        System.out.println("Jordan" == "Jordan");
+        System.out.println("JORDAN" == "jordan".toUpperCase());
+    }
+}
